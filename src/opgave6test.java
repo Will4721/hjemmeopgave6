cba18a3954e3fa73b22732e8f24b799e872c1a34
Will4721 <williamlehd@gmail.com>
2026-02-09 14:41:41 +0100

    first

--- src/opgave6test.java
+++ src/opgave6test.java
@@ -1,2 +1,9 @@
 public class opgave6test {
-}
+    public static void main(String[] args) {
+        int action = 0;
+        for(int i = 0; i<6;i++){
+            spin();
+        }
+    }
+    }
+
