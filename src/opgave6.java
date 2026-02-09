8a04b452c0e70faa0836379f333c2501e72a0b3e
Will4721 <williamlehd@gmail.com>
2026-02-09 15:23:48 +0100

    advanced

--- src/opgave6.java
+++ src/opgave6.java
@@ -3,16 +3,40 @@ import java.util.Random;
 public class opgave6 {
     Random random;
     int action = 0;
-
+     int p = 1;
     public opgave6() {
         this.random = new Random();
     }
 
-    public void spin(){
+        public void spin () {
+        if(p == 1){
         int result = random.nextInt(6);
-        System.out.println(result);
+        if (result == 0) {
+            System.out.println("Eating breakfeast");
+        }
+        if (result == 1) {
+            System.out.println("Study something boring");
+        }
+        if (result == 2) {
+            System.out.println("Go swimming");
+        }
+        if (result == 3) {
+            System.out.println("Go fishing");
+        }
+        if (result == 4) {
+            System.out.println("call your mom");
+        }
+        if (result == 5) {
+            System.out.println("Go back to bed");
+            action++;
+        }
+        if (action > 3) {
+            p = 0;
+            System.out.println("he was lazy and went back too bed for the rest of the day");
+
+        }
 
     }
+    }
 }
-
 
--- src/opgave6test.java
+++ src/opgave6test.java
@@ -4,7 +4,7 @@ public class opgave6test {
     public static void main(String[] args) {
             opgave6 hej = new opgave6();
 
-        for(int i = 0; i<6;i++){
+        for(int i = 0; i<5;i++){
             hej.spin();
 
         }
